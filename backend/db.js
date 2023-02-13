a3df336d5e68062fc5f9593873e4d6419bcc6daa
skmdnurujjaman <smnideal@gmail.com>
2023-02-13 10:04:17 +0530

    added cors

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const mongoURI = process.env.mongoURI;
 
 const mongoDB = async () => {
-  await mongoose.connect(mongoURI, (err, res) => {
+  mongoose.set("strictQuery", false);
+  await mongoose.connect(mongoURI, async (err, res) => {
     if (err) {
       console.log("DB Not connected!");
     } else {
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 require("dotenv").config();
-
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
 const mongoDB = require("./db");
 mongoDB();
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
